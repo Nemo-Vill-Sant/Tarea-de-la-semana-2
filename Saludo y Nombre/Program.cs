c3b1177ab21834b76800f39281cf620b8c14aa57
Nemo-Vill-Sant <SMIS032621@ugb.edu.sv>
2021-08-02 17:19:00 -0600

    Agregar archivos de proyecto.

---
+++ Saludo y Nombre/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saludo_y_Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Brayan");
+
+            Console.WriteLine("Hola Arnold");
+
+            Console.WriteLine("Presione cualquier tecla para cerrar");
+
+            Console.ReadKey();
+        }
+    }
+}
